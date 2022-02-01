31f9b15fc97b26c91bc8f823e95df9056992fd45
keyzhaus <98558356+keyzhaus@users.noreply.github.com>
2022-02-01 12:05:38 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+const CONFIG = {
+  hour12  : true
+}
+
+const ICONS = [
+  {
+    icon: "ri-mail-line",
+    link: "https://www.gmail.com"
+  },
+  {
+    icon: "ri-bank-card-fill",
+    link: "https://www.ynab.com"
+  },
+  {
+    icon: "ri-stock-fill",
+    link: "https://www.robinhood.com/"
+  },
+]
+
+const LIST_ITEMS = [
+  {
+    name: "drive",
+    link: "https://drive.google.com"
+  },
+    {
+    name: "box",
+    link: "https://www.dropbox.com/"
+  },
+]
